2c5b3cfbb23febbf44ffd2d6dc6c719f7540b1a4
Elliot Yim <yimsb02@naver.com>
2024-05-30 21:00:59 +0900

    fix: Add descriptions for adapters

--- app/adapter/incoming/web/post.py
+++ app/adapter/incoming/web/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path
 from starlette.responses import JSONResponse
 
 from app.adapter.incoming.web.schema.request.post import (
@@ -13,60 +13,77 @@ from app.domain.service.crud import CRUDService
 router = APIRouter()
 
 
-@router.post("")
+@router.post("", response_model=Post)
 def create_post(
     body: RequestPostCreate,
     crud_service: CRUDService = Depends(
         crud_service_factory(entity.Post, PostCreate, PostUpdate)
     ),
 ):
+    """
+    Create a post.
+    """
     post = crud_service.create(PostCreate.from_orm(body))
     return JSONResponse(
         status_code=201, content=Post.model_validate(post).model_dump(mode="json")
     )
 
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=Post)
 def get_post(
-    id: int,
+    id: int = Path(..., description="Post ID"),
     crud_service: CRUDService = Depends(
         crud_service_factory(entity.Post, PostCreate, PostUpdate)
     ),
 ):
+    """
+    Get a post.
+    """
     post = crud_service.retrieve(id)
     return JSONResponse(content=Post.model_validate(post).model_dump(mode="json"))
 
 
-@router.patch("/{id}")
+@router.patch("/{id}", response_model=Post)
 def patch_post(
-    id: int,
     body: RequestPostUpdate,
+    id: int = Path(..., description="Post ID"),
     crud_service: CRUDService = Depends(
         crud_service_factory(entity.Post, PostCreate, PostUpdate)
     ),
 ):
+    """
+    Update a post partially.\n
+    Any null field of body won't be changed.
+    """
     post = crud_service.patch(pk=id, update_schema=PostUpdate.from_orm(body))
     return JSONResponse(content=Post.model_validate(post).model_dump(mode="json"))
 
 
-@router.put("/{id}")
+@router.put("/{id}", response_model=Post)
 def put_post(
-    id: int,
     body: RequestPostUpdate,
+    id: int = Path(..., description="Post ID"),
     crud_service: CRUDService = Depends(
         crud_service_factory(entity.Post, PostCreate, PostUpdate)
     ),
 ):
+    """
+    Update a post.\n
+    If any field of body set null, it will be changed as null.
+    """
     post = crud_service.put(pk=id, update_schema=PostUpdate.from_orm(body))
     return JSONResponse(content=Post.model_validate(post).model_dump(mode="json"))
 
 
 @router.delete("/{id}")
 def delete_post(
-    id: int,
+    id: int = Path(..., description="Post ID"),
     crud_service: CRUDService = Depends(
         crud_service_factory(entity.Post, PostCreate, PostUpdate)
     ),
 ):
+    """
+    Delete a post.
+    """
     crud_service.delete(pk=id)
     return JSONResponse(status_code=204, content=None)
--- app/adapter/incoming/web/schema/request/post.py
+++ app/adapter/incoming/web/schema/request/post.py
@@ -1,12 +1,13 @@
+from fastapi import Body
 from pydantic import BaseModel
 
 
 class RequestPostCreate(BaseModel):
-    title: str
-    content: str
-    author_id: str
+    title: str = Body(..., description="Post Title")
+    content: str = Body(..., description="Post Content")
+    author_id: str = Body(..., description="Author ID")
 
 
 class RequestPostUpdate(BaseModel):
-    title: str | None = None
-    content: str | None = None
+    title: str | None = Body(None, description="Post Title")
+    content: str | None = Body(None, description="Post Content")
